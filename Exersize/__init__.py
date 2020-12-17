5023be7c43dbbb632a5b5c141843b4ebfe607a37
crashmatt <uavflightdirector@gmail.com>
2020-12-17 01:19:18 +0100

    Added new exersize types. Untested.

--- Exersize/__init__.py
+++ Exersize/__init__.py
@@ -7,6 +7,8 @@ Created on 12.12.2020
 name = "Exersize"
 desc = "Exersize patterns and updates"
 
+import math
+
 def showInfo():
     print(name + " - " + desc)
     
@@ -25,3 +27,42 @@ class Exersize():
   def update_setting(self, position, velocity, force_N):
     return ExersizeSetting(force_target=30)
     
+
+class ElasticExersize(Exersize):
+  def __init__(self, Kspring=100, preload=25, Fmax=100):
+    self.Kspring = Kspring
+    self.Fmax = Fmax
+    self.preload = preload
+    
+  def update_setting(self, position, velocity, force_N):
+    force = self.preload + ( math.fabs(position) * self.Kspring)
+    if force > self.Fmax:
+      force = self.Fmax
+    return ExersizeSetting(force_target=force)
+
+
+
+class MassExersize(Exersize):
+  def __init__(self, mass=10.0, static_mass=1.0, t_dvfilt=0.1, Fmax=150):
+    self.mass = mass
+    self.static_mass = static_mass
+    self.filtered_delta_velocity = 0
+    self.t_dvfilt = t_dvfilt
+    self.last_velocity = 0
+    self.Fmax = Fmax
+    
+  def update_setting(self, position, velocity, force_N):
+    delta_v = velocity - self.last_velocity
+    ddv = self.filtered_delta_velocity - delta_v
+    ddv_filt = ddv * self.t_dvfilt
+    self.filtered_delta_velocity += ddv_filt
+    
+    accel = 9.8 + self.filtered_delta_velocity
+    force = accel * mass
+    
+    if force > self.Fmax:
+      force = self.Fmax
+
+    self.last_velocity = velocity
+
+    return ExersizeSetting(force_target=force, acceleration_target=9.8)
